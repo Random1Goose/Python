1fc8831cf2d5ed23a199afd1aabb99e6f97e3d43
Random1Goose <144254870+Random1Goose@users.noreply.github.com>
2024-01-25 10:36:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+password = 1337
+password = int(input('Guess the password '))
+if password == 1337:
+  print('Password correct')
+if password > 1337:
+  print('Password Incorrect')
+
+if password < 1337:
+  print('Password Incorrect')
+
+NO = int(input('Enter a Number '))
+while NO >= 0:
+  print(NO)
+  NO-=1
